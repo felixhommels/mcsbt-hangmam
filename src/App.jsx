a0324bd121ae531dc08102ad9f8304ab6e52b67e
Felix Hommels <felixhommels@gmail.com>
2025-02-12 22:30:06 +0100

    Initial app.jsx combination

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
 import Header from "./components/Header";
+import Hangman from "./components/Hangman";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
-  return <Header />;
+  return (
+    <Container fluid className="px-0">
+      <Row>
+        <Col>
+          <Header />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Hangman />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/Hangman.jsx
@@ -0,0 +1,5 @@
+function Hangman() {
+  return <h1>Hello</h1>;
+}
+
+export default Hangman;
